87f441241734f12820354455ea2525a4248168e0
plamen1982 <plamen.hristov@ymail.com>
2017-01-29 16:27:22 -0500

    Fix path for chai-http and app

--- test/heroId.test.ts
+++ test/heroId.test.ts
@@ -1,26 +1,29 @@
 import * as mocha from 'mocha'
 import * as chai from 'chai'
-import * as chaiHttp from 'http-chai'
+import  chaiHttp = require('chai-http')
 
-import app from '../scr/App'
+import app from '../src/App'
 
 chai.use(chaiHttp);
 
 const expect = chai.expect;
 
 describe('GET api/v1/heroes/:id', () => {
-    it('responds with single JSON object', () =>{
+
+    it('responds with single JSON object', () => {
         return chai.request(app).get('/api/v1/heroes/1')
             .then(res => {
                 expect(res.status).to.equal(200);
                 expect(res).to.be.json;
-                expect(res.body).to.be.an('object')
-            } )
-    })
+                expect(res.body).to.be.an('object');
+            });
+    });
+
     it('should return Luke Cage', () => {
-        return chai.request.get('/api/v1/heroes/1')
-            .then( res=> {
-                expect(res.body.hero.name).to.equal('Luke Cage')
-            })
-    })
-})
+        return chai.request(app).get('/api/v1/heroes/1')
+            .then(res => {
+                expect(res.body.hero.name).to.equal('Luke Cage');
+            });
+    });
+
+});
